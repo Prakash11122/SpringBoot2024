0565f70add6a6cb7a342f4403d5f5297181b1c2e
prakash kumar pradhan <111282629+Prakash11122@users.noreply.github.com>
2024-11-02 23:11:28 +0530

    SpringBoot profile Example 3

---
+++ 09SpringBootProfile3/src/main/java/com/it/pradhan/runner/DbUserSetupRunner.java
@@ -0,0 +1,20 @@
+package com.it.pradhan.runner;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Component
+//@Profile("default")
+@Profile({"qa","default","uat"})
+public class DbUserSetupRunner implements CommandLineRunner{
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("FROM DbUser SETUP RUNNER");
+		
+	}
+	
+	
+
+}
